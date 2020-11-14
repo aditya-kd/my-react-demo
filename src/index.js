6ad19bf005fe5514fd985795ed4509ea29be480f
Aditya Sharma <aditya.sharma.cms17@gmail.com>
2020-11-14 11:31:03 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<h1>Hello World! This is aditya!</h1>,document.getElementById("root"))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
